b95422da2d59b18e93609fd81d8e38b4668cd43a
sairevanth0406 <sairevanth040@gmail.com>
2025-12-07 14:52:16 +0530

    Update app to use environment variable for port

--- frontend_flask/app.py
+++ frontend_flask/app.py
@@ -188,4 +188,6 @@ def logout():
 
 # RUN APP ----------------------------
 if __name__ == "__main__":
-    app.run(debug=True, port=10000)
+
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
